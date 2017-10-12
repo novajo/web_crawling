a05b3f30c899c8e3068c2a55cb62241b10813e1e
novajo <novajo@gmail.com>
2017-10-12 16:58:26 +0800

    add csv writer

--- 从零开始写Python爬虫/1.7 novel.py
+++ 从零开始写Python爬虫/1.7 novel.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import requests
-import datetime
 from scrapy import Selector
 import csv
 
@@ -10,33 +9,34 @@ def get_sel(url):
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
     }
     r = requests.get(url, headers=headers)
-    print(r.status_code)
+    # print(r.status_code)
     sel = Selector(text=r.content, type='html')
     return sel
 
 
+list_title = []
+list_link = []
 seed_url = 'http://www.qu.la/paihangbang/'
 sel = get_sel(seed_url)
-cnt = 0
 for se in sel.xpath("//div[contains(@class, 'index_toplist')]//div[contains(@id, 'con') and contains(@id, 'g_1')]//li/a[starts-with(@href, '/book/')]"):
-    text = se.xpath("text()").extract()[0]
+    title = se.xpath("text()").extract()[0]
     link = se.xpath("@href").extract()[0]
-    print(str(text)+" http://www.qu.la/paihangbang"+str(link))
-    cnt += 1
-print(cnt)
-'''
-for se in sel.xpath('//div[@class="topbooks" and @style="display: block;"]/ul/li/a'):
-    s = Selector(text=se.extract(), type='html')
-    internal_link = s.xpath('//@href').extract_first()
-    title = s.xpath('//@title').extract_first()
-    download_link = seed_url + str(internal_link)
-    with open('novel_list.csv', 'a') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.write(title.encode('utf-8'))
-        writer.write(download_link)
-        csvfile.close()
-        '''
+    # print(str(title)+" http://www.qu.la/paihangbang"+str(link))
+    if title not in list_title:
+        list_title.append(title)
+        list_link.append(link)
 
+try:
+    with open('novel_list.csv', 'w', newline='') as csvfile:
+        fieldnames = ["title", "link"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for i in range(0, len(list_title)):
+            # print(list_title[i])
+            writer.writerow({'title': list_title[i], 'link': (seed_url+list_link[i])})
+        csvfile.close()
+except PermissionError as e:
+    print('Permission Denied')
 
 
 
