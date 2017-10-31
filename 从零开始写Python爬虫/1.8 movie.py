a5595f9b95dcb84119e209fbf807a2b98cd0d1e9
novajo <novajo@gmail.com>
2017-10-31 16:34:24 +0800

    update img download module

--- 从零开始写Python爬虫/1.8 movie.py
+++ 从零开始写Python爬虫/1.8 movie.py
@@ -13,16 +13,27 @@ def get_sel(url):
     return selector
 
 
+def download_img(url, name):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
+    }
+    r = requests.get(url, headers=headers)
+    with open('c:/Users/wniu/Desktop/img/' + name + '.png', 'wb+') as f:
+        f.write(r.content)
+
+
 def get_Movies():
     url = 'http://dianying.2345.com/top/'
     sel = get_sel(url)
-    section = sel.xpath('//ul[@class="picList clearfix"]/li/div[@class="txt"]')
+    section = sel.xpath('//ul[@class="picList clearfix"]/li')
     #title = section.xpath('//span[@class="sTit"]/a/text()').extract()
     for s in section:
         # print(s)
-        title = s.xpath('p[@class="pTit"]/span[@class="sTit"]/a/text()').extract_first()
-        starrings = s.xpath('p[@class="pActor"]/a/text()').extract()
-        intro = s.xpath('p[@class="pTxt pIntroShow"]/text()').extract_first()
+        title = s.xpath('div[@class="txt"]/p[@class="pTit"]/span[@class="sTit"]/a/text()').extract_first()
+        starrings = s.xpath('div[@class="txt"]/p[@class="pActor"]/a/text()').extract()
+        intro = s.xpath('div[@class="txt"]/p[@class="pTxt pIntroShow"]/text()').extract_first()
+        img_url = "http:"+s.xpath('div[@class="pic"]/img/@src').extract_first()
+        download_img(img_url, title)
         # if title is not None:
         print("片名: "+title)
         print("主演: ")
